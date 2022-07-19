c001a0778861bc628d75a81009d140ace22a9f68
quantumsoul <prakhar24012000@gmail.com>
2022-07-20 02:05:37 +0530

    init commit

---
+++ routes/route.js
@@ -0,0 +1,47 @@
+const express = require("express")
+const router = express.Router()
+const uploadMedia = require('../config/multer.config')
+router.post("/pdf/link",uploadMedia.single('file'),async(req,res)=>{
+    try {
+        const AWS = require('aws-sdk');
+        const fs = require('fs');
+
+        const AWSCredentials = {
+            accessKey: process.env.accessKey,
+            secret: process.env.secret,
+            bucketName: 'awsbucket-project'
+        };
+
+        const s3 = new AWS.S3({
+            accessKeyId: AWSCredentials.accessKey,
+            secretAccessKey: AWSCredentials.secret
+        });
+        if(req.file){
+            let file = req.file;
+            // Read content from the file
+            // const fileContent = fs.readFileSync(file.buffer);
+
+            // Setting up S3 upload parameters
+            const params = {
+                Bucket: AWSCredentials.bucketName,
+                Key: file.originalname,
+                Body: file.buffer
+            };
+
+            // Uploading files to the bucket
+            s3.upload(params, function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                res.json({msg:"pdf link send successfully", link:`${data.Location}`});
+            });
+        } else{
+            throw new Error("File not found!")
+        }
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        })
+    }
+})
+module.exports = router
