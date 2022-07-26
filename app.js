10faee55c1514cccb6e194711f11c806cfc1208c
quantumsoul <prakhar24012000@gmail.com>
2022-07-26 20:07:53 +0530

    cors errror resolve

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const Router = require("./routes/route");
 const app = express();
+app.use((req, res, next) => {
+    // allow different IP address
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    // allow different header field 
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS');
+  
+    next();
+  })
 app.use(express.json());
 app.use(Router);
 module.exports = app;
--- server.js
+++ server.js
@@ -4,15 +4,6 @@ dotenv.config({ path: "./config.env" });
 
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
-app.use((req, res, next) => {
-  // allow different IP address
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  // allow different header field 
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, PUT, GET, OPTIONS');
-
-  next();
-})
 app.listen(PORT, () => {
   console.log("Server listening on port", PORT);
 });
