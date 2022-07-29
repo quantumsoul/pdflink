69cd02a61ce0548db3dad609c989ad49d53f3379
Gyasuddin Ansari <56863773+Gya7329@users.noreply.github.com>
2022-07-29 18:35:32 +0530

    Update route.js

--- routes/route.js
+++ routes/route.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const uploadMedia = require('../config/multer.config')
-router.post("/api/v2/pdf/link",uploadMedia.single('file'),async(req,res)=>{
+router.post("/api/v2/upload/file",uploadMedia.single('file'),async(req,res)=>{
     try {
         const AWS = require('aws-sdk');
         const fs = require('fs');
@@ -27,8 +27,52 @@ router.post("/api/v2/pdf/link",uploadMedia.single('file'),async(req,res)=>{
                 Key: file.originalname,
                 Body: file.buffer,
                 ACL:'public-read',
-                ContentType:"application/octet-stream"
-            };
+ };
+
+            // Uploading files to the bucket
+            s3.upload(params, function(err, data) {
+                if (err) {
+                    throw err;
+                }
+                res.json({msg:"pdf link send successfully", link:`${data.Location}`});
+            });
+        } else{
+            throw new Error("File not found!")
+        }
+    } catch (error) {
+        res.status(400).json({
+            error: error.message
+        })
+    }
+})
+router.post("/api/v2/upload/pdf",uploadMedia.single('file'),async(req,res)=>{
+    try {
+        const AWS = require('aws-sdk');
+        const fs = require('fs');
+
+        const AWSCredentials = {
+            accessKey: process.env.accessKey,
+            secret: process.env.secret,
+            bucketName: 'awsbucket-project'
+        };
+
+        const s3 = new AWS.S3({
+            accessKeyId: AWSCredentials.accessKey,
+            secretAccessKey: AWSCredentials.secret
+        });
+        if(req.file){
+            let file = req.file;
+            // Read content from the file
+            // const fileContent = fs.readFileSync(file.buffer);
+
+            // Setting up S3 upload parameters
+            const params = {
+                Bucket: AWSCredentials.bucketName,
+                Key: file.originalname,
+                Body: file.buffer,
+                ACL:'public-read',
+                ContentType:"application/pdf" 
+ };
 
             // Uploading files to the bucket
             s3.upload(params, function(err, data) {
